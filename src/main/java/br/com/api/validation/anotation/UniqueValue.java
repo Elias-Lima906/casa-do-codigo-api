4740cb172a3e9904ede54f06503576e7b29e334f
Elias Lima <eliaslima900@hotmail.com>
2021-02-19 19:16:20 -0300

    create generic anotation for unique constraint

--- src/main/java/br/com/api/validation/anotation/UniqueValue.java
+++ src/main/java/br/com/api/validation/anotation/UniqueValue.java
@@ -9,17 +9,21 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
-import br.com.api.validation.validator.EmailValidation;
+import br.com.api.validation.validator.UniqueValueValidator;
 
 @Documented
-@Constraint(validatedBy = EmailValidation.class)
+@Constraint(validatedBy = {UniqueValueValidator.class})
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface UniqueEmail {
+public @interface UniqueValue {
 
 	String message() default "{br.com.api.validation.anotation.UniqueEmail}";
 	
 	Class<?>[] groups() default { };
 	
 	Class<? extends Payload>[] payload() default { };
+	
+	String fieldName();
+	
+	Class<?> 	domainName();
 }
