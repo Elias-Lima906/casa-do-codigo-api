f70764904152b3ada55e34c3b85ce0b2a92b4e7f
Elias Lima <eliaslima900@hotmail.com>
2021-02-22 17:01:09 -0300

    create a method to find a state by id

--- src/main/java/br/com/api/cdc/state/State.java
+++ src/main/java/br/com/api/cdc/state/State.java
@@ -1,18 +1,23 @@
 package br.com.api.cdc.state;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Query;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import br.com.api.cdc.country.Country;
+import br.com.api.cdc.exception.GlobalException;
 
 @Entity
 public class State {
@@ -54,6 +59,23 @@ public class State {
 	
 	public Long getIdCountry() {
 		return country.getId();
+	}
+	
+	public static State findStateById(Long idState, EntityManager manager) throws GlobalException {
+		
+		List<?> list = queryResult(idState, manager);
+		
+		if(list.isEmpty()) {
+			throw new GlobalException("O Estado não existe no banco de dados pelo id: " + idState + ", por favor passe um id valido!");
+		}
+		
+		return (State) list.get(0);
+	}
+	
+	private static List<?> queryResult(Long idState, EntityManager manager){
+		Query query = manager.createQuery("SELECT s FROM State s WHERE s.id = :idState");	
+		query.setParameter("idState", idState);
+		return query.getResultList();
 	}
 
 }
