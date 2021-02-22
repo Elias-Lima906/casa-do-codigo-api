32a898e6ee153e954033c3a225e23d40f27b942a
Elias Lima <eliaslima900@hotmail.com>
2021-02-22 13:44:48 -0300

    create query to check if state exists by name per country

--- src/main/java/br/com/api/cdc/state/StateController.java
+++ src/main/java/br/com/api/cdc/state/StateController.java
@@ -5,6 +5,7 @@ import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +20,15 @@ public class StateController {
 	
 	@PostMapping
 	@Transactional
-	public StateResponseDTO saveState(@RequestBody @Valid StateRequestDTO request) {
+	public ResponseEntity<StateResponseDTO> saveState(@RequestBody @Valid StateRequestDTO request) {
+
+		if(request.checkIfStatesNameExistsInCountry(manager)) {
+			return ResponseEntity.badRequest().build();
+		}
 
 		@Valid State state = request.toModel(manager);
 		manager.persist(state);
-		return new StateResponseDTO(state);
+		return ResponseEntity.ok(new StateResponseDTO(state));
 	}
 
 }
