6d413c2853d0f7b3230416b7690da079a4c23ad3
Elias Lima <eliaslima900@hotmail.com>
2021-02-22 14:39:01 -0300

    create bidirectional relation with state

--- src/main/java/br/com/api/cdc/country/Country.java
+++ src/main/java/br/com/api/cdc/country/Country.java
@@ -1,11 +1,23 @@
 package br.com.api.cdc.country;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Query;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import br.com.api.cdc.state.State;
 
 @Entity
 public class Country {
@@ -18,10 +30,14 @@ public class Country {
 	@Column(nullable = false, unique = true)
 	private String name;
 
+	@JsonManagedReference
+	@OneToMany(mappedBy = "country", fetch = FetchType.EAGER)
+	private List<State> states = Arrays.asList();
+
 	@Deprecated
 	public Country() {
 	}
-	
+
 	public Country(@NotBlank String name) {
 		this.name = name;
 	}
@@ -32,6 +48,26 @@ public class Country {
 
 	public String getName() {
 		return name;
+	}
+
+	public List<State> getStates() {
+		return states;
+	}
+
+	public void setStates(List<State> states) {
+		this.states = states;
+	}
+
+	public static void addState(@Valid State state, EntityManager manager) {
+
+		Country country = manager.find(Country.class, state.getIdCountry());
+
+		country.setStates(Arrays.asList(state));
+	}
+
+	public static List<Country> findAllCountries(EntityManager manager) {
+		Query query = manager.createQuery("SELECT c FROM Country c");
+		return query.getResultList();
 	}
 
 }
